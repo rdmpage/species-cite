d70b7db50560cbb8da9ccdd03aef681085f00377
Roderic Page <rdmpage@gmail.com>
2021-04-26 14:43:08 +0100

    Redesign and add PDF display

--- index.php
+++ index.php
@@ -18,9 +18,6 @@ if (isset($_GET['q']))
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<style>
-		body {
-			padding:20px;
-		}
 		</style>
 		
 		<script src="js/jquery.js" type="text/javascript"></script>		
@@ -65,40 +62,57 @@ if (isset($_GET['q']))
 					
 				element.innerHTML = output;			
 			}
-		</script>		
+		</script>	
+		
+		<script>
+		// Display PDF
+		function display_pdf(element_id, pdf_url, page) {
+			$('#' + element_id).html("");
+		
+			var page_to_display = page || 1;
+			
+			var pdfjs_url 	= 'pdfjs/web/viewer.html?file=';
+			//pdfjs_url 		= 'pdf.js-hypothes.is/viewer/web/viewer.html?file=';
+			
+			var proxy_url 	= '../../pdfproxy.php?url=';
+			
+			var html = '<iframe id="pdf" width="100%" height="700" src="' 
+				+ pdfjs_url
+				+ encodeURIComponent(proxy_url + encodeURIComponent(pdf_url))
+				
+				 + '#page=' + page_to_display + '"/>';
+				 
+			$('#' + element_id).html(html);
+	
+		}
+		
+
+		</script>			
 		
 	</head>
 	<body>
-	<div class="container">
-	<h1>Species Cite<small>Find taxonomic names, where they were published, and by whom.</small></h1>
-
-	<p>A <a href="https://github.com/rdmpage/species-cite">project</a> by Rod Page.</p>
 	
-	<form action=".">
-	<div class="row">
-  <input type="search" style="width:80%;" id="q" name="q" placeholder="species name" value="<?php echo $q; ?>"/>
-  <input class="primary" type="submit" value="Search" />
-  </div>
-	</form>
-
-<!--
-<div class="row">
-	<div class="col-sm-12">
-		<div class="card fluid" style="margin: 0.5rem 0.25rem">
-			<h3 class="doc section">Title section</h3>
-			<p class="doc section">This is a section with some textual content. <span class="icon-link"></span> <a href="#" class="button">PDF</a></p>
-		</div>
-	</div>
-</div>
--->
-
+	<div class="container">
+		<header class="row">
+			<span class="logo col-sm-3 col-md">Species Cite</span>
+		</header>
+		<div class="row">
+			<div class="col-sm-12 col-md-4">
+				
+				<form action=".">
+				<div class="row">
+				<input type="search" id="q" name="q" placeholder="species name" value="<?php echo $q; ?>"/>
+				<input class="primary" type="submit" value="Search" />
+				</div>
+				</form>	
+				
 <?php
 
 	if ($q != '')
 	{
 		// do stuff here...
 		
-		$results = do_search($q);
+		$results = do_search(trim($q));
 		
 		if (0)
 		{
@@ -178,11 +192,28 @@ if (isset($_GET['q']))
 					echo '<p class="doc section">';
 					
 					echo '<a href="https://doi.org/' .$result->publication->doi . '" target="_new">https://doi.org/' . $result->publication->doi . '</a><span class="icon-link"></span>';
-					
-					echo '<span id="unpaywall_' . $counter . '" class="unpaywall" data="' . $result->publication->doi . '"></span>';
-					
+										
 					echo '</p>';
 				}
+				
+				// View
+				echo '<p class="doc section">';
+				
+				// Do we already know that we have a PDF? If so, make a button
+				if (isset($result->publication->pdf))
+				{
+					// echo '<a href="' . $result->publication->pdf[0] . '">PDF</a>';					
+					echo '<button tertiary onclick="onclick=display_pdf(\'viewer\', \'' . $result->publication->pdf[0] . '\')">View</button>';
+				}
+				else
+				{
+					if (isset($result->publication->doi))
+					{
+						echo '<button disabled class="unpayall" id="unpaywall_' . $counter . '" data="' . $result->publication->doi . '">View</button>';
+					}
+				}
+				echo '</p>';
+				
 			}
 		
 			if (isset($result->people))
@@ -203,22 +234,21 @@ if (isset($_GET['q']))
 		
 		echo '
 				// code to call unpaywall
-				$( "span.unpaywall" ).each(function() {
-  					
-  					
+				$( "button" ).each(function() {
+   					
   					var id = $(this).attr("id");
   					var doi = $(this).attr("data");
-  					
-  					
   					
   					var url = "https://api.oadoi.org/v2/" + encodeURIComponent(doi) 
   						+ "?email=unpaywall@impactstory.org" ;
   					
   					$.getJSON(url,
 						function(data){
-							//console.log(data);
+							console.log(data);
 							if (data.is_oa) {
-								$("#" + id).html("<a class=\"button tertiary\" href=\"" + data.oa_locations[0].url_for_pdf + "\" target=\"_new\">&nbsp;Read for free&nbsp;</a>");
+								$("#" + id).removeAttr("disabled");
+								$("#" + id).html("View (via Unpaywall)");
+								$("#" + id).attr("onclick", "display_pdf(\'viewer\', \'" + data.oa_locations[0].url_for_pdf + "\')");
 							}
 						}
 					);
@@ -233,8 +263,15 @@ if (isset($_GET['q']))
 		
 	}
 	
-?>
+?>						
+			
+			</div>
+			<div class="col-sm-12  col-md-8">
+				<div id="viewer"></div>
+			</div>
+		</div>
 	</div>
+	
 	</body>
 </html>
 
